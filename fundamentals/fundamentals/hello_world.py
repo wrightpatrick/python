fc05613d1eb5ed7f6720e765603c600cccddf483
Patrick Wright <patrickdane@me.com>
2022-01-10 13:39:03 -0500

    hello world assignment

---
+++ fundamentals/fundamentals/hello_world.py
@@ -0,0 +1,16 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print(f"Hello {name}")	# with a comma
+#print( your code here )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print( f"Hello {name}!")	# with a comma
+# print( your code here )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( f"I love to eat {fave_food1} and {fave_food2}." ) # with .format()
+# print( your code here ) # with an f string
+
