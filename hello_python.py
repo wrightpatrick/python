4d2c41d288bc01a8a7a6432af85d7b9ff1080d5f
Patrick Wright <patrickdane@me.com>
2022-01-10 13:02:11 -0500

    hello python lecture 1

--- hello_python.py
+++ hello_python.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+# def reverseString(str):
+#     new_list = []
+
+#     for i in range(0, len(str), 1):
+#         new_list.append(str[i])
+
+#     if len(new_list) < 1:
+#         print("string is too short")
+    
+#     print(new_list)
+# print("hello world!")
+
+# reverseString("apple pie is good")
+
+# import random
+# print(random.randint(1,20))
+
+# string = f"hello my age is {random.randint(1,100)}"
+
+# print(string)
+
