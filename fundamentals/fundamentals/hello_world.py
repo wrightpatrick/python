009c5a95aadd5e13ab01542781e06b4e11ed8e4c
Patrick Wright <patrickdane@me.com>
2022-01-10 13:59:43 -0500

    updated

--- fundamentals/fundamentals/hello_world.py
+++ fundamentals/fundamentals/hello_world.py
@@ -1,16 +1,16 @@
 # 1. TASK: print "Hello World"
 print("Hello World")
 # 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
-print(f"Hello {name}")	# with a comma
+name = "Patrick"
+print(f"Hello {name}!")	# with a comma
 #print( your code here )	# with a +
 # 3. print "Hello 42!" with the number in a variable
 name = 42
-print( f"Hello {name}!")	# with a comma
-# print( your code here )	# with a +	-- this one should give us an error!
+print( "Hello", name, "!")	# with a comma
+print( "Hello " + str(name) + " !")	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 print( f"I love to eat {fave_food1} and {fave_food2}." ) # with .format()
-# print( your code here ) # with an f string
+print( "I love to eat {} and {}.".format(fave_food1, fave_food2)) # with an f string
 
