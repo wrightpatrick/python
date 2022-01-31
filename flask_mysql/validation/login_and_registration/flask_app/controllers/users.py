e7bf24c5545f9c324afad4d31636b2fcdf9d81e8
Patrick Wright <patrickdane@me.com>
2022-01-30 22:45:35 -0500

    update

--- flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
+++ flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
@@ -37,7 +37,8 @@ def register_user():
 # dashboard landing page ========================================================
 @app.route("/dashboard")
 def dashboard():
-	if session == []:
+	if "user_id" not in session:
+		flash("Please login to access the dashboard.")
 		return redirect("/")
 	
 	data = {
